15e5297fcfa300014f8a2fb6f5a16f7c70218271
Kimberly MB <k.mueses@yahoo.com>
2023-02-06 14:47:00 -0500

    Creates outline for email admin class

---
+++ src/emailapp/Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+//    Constructor will receive the first and last name
+
+//    Will need to get the department
+
+//    Will need to generate a random password
+
+//    Will need to set the mailbox capacity
+
+//    Will need to set alternate email
+
+//    Will need to change the password
+}
