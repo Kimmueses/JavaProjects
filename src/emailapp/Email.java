1dab7dfac618a2a68a338f8a54939b530629c100
Kimberly MB <k.mueses@yahoo.com>
2023-02-07 16:04:05 -0500

    Created method to display information

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -17,11 +17,11 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+//        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 
 //    Call a method to ask for department & return it
         this.department = setDepartment();
-        System.out.println("Department: " + this.department);
+//        System.out.println("Department: " + this.department);
 
 //    Call a method that returns random password
         this.password = randomPassword(defaultPasswordLength);
@@ -29,11 +29,11 @@ public class Email {
 
 //    Combine elements to generate email
         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-        System.out.println("Your email is: " + email);
+//        System.out.println("Your email is: " + email);
     }
 //    Will need to get the department
     private String setDepartment() {
-        System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        System.out.println("New Employee: " + firstName + " " + lastName + "\nDEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
         if (depChoice == 1) {
@@ -81,6 +81,12 @@ public class Email {
 
     public String getPassword() {
         return password;
+    }
+
+    public String showInfo() {
+        return "DISPLAY NAME: " + firstName + " " + lastName +
+                "\nCOMPANY EMAIL: " + email +
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
     }
 }
 
