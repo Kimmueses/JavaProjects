e4780fc3f7026b94ac84d0b5b8d0b7c56c3cc416
Kimberly MB <k.mueses@yahoo.com>
2023-02-07 14:37:33 -0500

    Defined email dept & created method for dept choice

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,9 +15,22 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+//    Call a method to ask for department & return it
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
 //    Will need to get the department
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "sales"; }
+        else if (depChoice == 2) {return "dev"; }
+        else if (depChoice == 3) {return "accounting"; }
+        else { return "";}
+
 
 //    Will need to generate a random password
 
@@ -24,4 +39,5 @@ public class Email {
 //    Will need to set alternate email
 
 //    Will need to change the password
+}
 }
